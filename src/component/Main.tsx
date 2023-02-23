8e344e70fd75ef1a72f07e764296b9e155c01a99
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2023-02-23 18:47:55 +0530

    final

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -9,15 +9,21 @@ const Main = () => {
   return (
     <div className="container">
       <header>
+        {/* rendering of header component */}
         <Header />
       </header>
       <main>
+        {/* rendering of banner component */}
         <Banner />
+        {/* rendering of items component */}
         <ItemsComp str="Our Top Deals" />
+        {/* rendering of items component */}
         <ItemsComp str="Best Selling Products" />
+        {/* rendering of subscribe component */}
         <SubscribeComp />
       </main>
       <footer>
+        {/* rendering of footer component */}
         <Footer />
       </footer>
     </div>
